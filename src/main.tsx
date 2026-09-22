f5b7f2eede97307f5da375ef19978f7faf060aa3
sambak07 <sambaraik@gmail.com>
2026-09-22 13:40:32 +0600

    feat: establish Money Saathi clean foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
+﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
+import './styles/global.css'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Root element was not found')
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
